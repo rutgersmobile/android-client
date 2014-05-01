84c1d6eff5f4002c02ae31a3e814ba5125b902f8
jachambers <jameschambers2@gmail.com>
2014-05-01 16:24:16 -0400

    Autocompletion for Places field

--- src/edu/rutgers/css/Rutgers/fragments/PlacesMain.java
+++ src/edu/rutgers/css/Rutgers/fragments/PlacesMain.java
@@ -1,11 +1,22 @@
 package edu.rutgers.css.Rutgers.fragments;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.jdeferred.DoneCallback;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.rutgers.css.Rutgers.api.Request;
 import edu.rutgers.css.Rutgers2.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 /**
  * Places channel fragment.
@@ -14,6 +25,12 @@ import android.view.ViewGroup;
  */
 public class PlacesMain extends Fragment {
 
+	private static final String TAG = "PlacesMain";
+	private static final String API_URL = "https://rumobile.rutgers.edu/1/places.txt";
+	private ArrayList<String> mList;
+	private ArrayAdapter<String> mAdapter;
+	private JSONObject mData;
+	
 	public PlacesMain() {
 		// Required empty constructor
 	}
@@ -21,6 +38,38 @@ public class PlacesMain extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		mList = new ArrayList<String>();
+		mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_dropdown_item_1line, mList);
+		
+		// Get places data
+		Request.json(API_URL).done(new DoneCallback<JSONObject>() {
+
+			@Override
+			public void onDone(JSONObject json) {
+				mData = json;
+				
+				// Grab "all" field and add title from each object inside
+				try {
+					JSONObject all = json.getJSONObject("all");
+					Iterator<String> curKey = all.keys();
+					while(curKey.hasNext()) {
+						JSONObject curBuilding = all.getJSONObject(curKey.next());
+						mAdapter.add(curBuilding.getString("title"));
+					}
+				} catch (JSONException e) {
+					Log.e(TAG, Log.getStackTraceString(e));
+				}
+				
+			}
+			
+		});
+		
+		/*
+		mAdapter.add("Busch Campus Center");
+		mAdapter.add("Rutgers Campus Center");
+		mAdapter.add("Douglass Campus Center");
+		*/
 	}
 	
 	@Override
@@ -30,7 +79,10 @@ public class PlacesMain extends Fragment {
 		
 		if(args.get("title") != null)
 			getActivity().setTitle(args.getString("title"));
-		
+
+		AutoCompleteTextView autoComp = (AutoCompleteTextView) v.findViewById(R.id.buildingSearchField);
+		autoComp.setAdapter(mAdapter);
+
 		return v;
 	}
 	
