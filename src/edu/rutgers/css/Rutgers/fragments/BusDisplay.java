9040b9af184f860882159c978d8d313add68060d
jachambers <jameschambers2@gmail.com>
2014-06-03 11:57:48 -0400

    Bus prediction display auto-refreshes (but should be smoother)

--- src/edu/rutgers/css/Rutgers/fragments/BusDisplay.java
+++ src/edu/rutgers/css/Rutgers/fragments/BusDisplay.java
@@ -1,10 +1,13 @@
 package edu.rutgers.css.Rutgers.fragments;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.jdeferred.DoneCallback;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,13 +18,18 @@ import edu.rutgers.css.Rutgers.auxiliary.Prediction;
 import edu.rutgers.css.Rutgers.auxiliary.PredictionAdapter;
 import edu.rutgers.css.Rutgers2.R;
 
-public class BusDisplay extends Fragment {
+public class BusDisplay extends Fragment implements DoneCallback<ArrayList<Prediction>> {
 
 	private static final String TAG = "BusDisplay";
+	private enum Mode {ROUTE, STOP};
 	
 	private ArrayList<Prediction> mData;
 	private PredictionAdapter mAdapter;
 	private ListView mList;
+	private Mode mMode;
+	private String mTag;
+	private Handler mUpdateHandler;
+	private Runnable mUpdateRunnable;
 	
 	public BusDisplay() {
 		// Required empty public constructor
@@ -41,31 +49,31 @@ public class BusDisplay extends Fragment {
 		else {
 			getActivity().setTitle("Bus Display");
 		}
-	
-		if(args.getString("mode").equals("route")) {
-			Nextbus.routePredict("nb", args.getString("tag")).then(new DoneCallback<ArrayList<Prediction>>() {
-
-				@Override
-				public void onDone(ArrayList<Prediction> predictionArray) {
-					for(int i = 0; i < predictionArray.size(); i++) {
-						mAdapter.add(predictionArray.get(i));
-					}
-				}
-				
-			});
+		
+		if(args.getString("mode").equalsIgnoreCase("route")) {
+			mMode = Mode.ROUTE;
+			mTag = args.getString("tag");
 		}
-		else if(args.getString("mode").equals("stop")) {
-			Nextbus.stopPredict("nb", args.getString("title")).then(new DoneCallback<ArrayList<Prediction>>() {
-
-				@Override
-				public void onDone(ArrayList<Prediction> predictionArray) {
-					for(int i = 0; i < predictionArray.size(); i++) {
-						mAdapter.add(predictionArray.get(i));
-					}
-				}
-				
-			});
+		else if(args.getString("mode").equalsIgnoreCase("stop")) {
+			mMode = Mode.STOP;
+			mTag = args.getString("title");
 		}
+		
+		mUpdateHandler = new Handler();
+		mUpdateRunnable = new Runnable() {
+			@Override
+			public void run() {
+				loadPredictions();
+			}
+		};
+		
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				mUpdateHandler.post(mUpdateRunnable);
+			}
+		}, 0, 1000 * 60);
 		
 	}
 	
@@ -74,8 +82,29 @@ public class BusDisplay extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_busdisplay, parent, false);
 		mList = (ListView) v.findViewById(R.id.busDisplayList);
 		mList.setAdapter(mAdapter);
-		
+
 		return v;
+	}
+	
+	/**
+	 * Load prediction data
+	 */
+	private void loadPredictions() {
+		mAdapter.clear();
+		
+		if(mMode == Mode.ROUTE) {
+			Nextbus.routePredict("nb", mTag).then(this);
+		}
+		else if(mMode == Mode.STOP) {
+			Nextbus.stopPredict("nb", mTag).then(this);
+		}
+	}
+	
+	@Override
+	public void onDone(ArrayList<Prediction> predictionArray) {
+		for(Prediction p: predictionArray) {
+			mAdapter.add(p);
+		}
 	}
 	
 }
