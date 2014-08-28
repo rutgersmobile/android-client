38541cdb693354aa2e091de1300a459b45bba2cb
jachambers <jameschambers2@gmail.com>
2014-08-28 19:44:34 -0400

    Web: refresh button

--- app/src/main/java/edu/rutgers/css/Rutgers/fragments/WebDisplay.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/fragments/WebDisplay.java
@@ -162,6 +162,19 @@ public class WebDisplay extends Fragment {
 	}
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_refresh:
+                if(mWebView != null) {
+                    mWebView.loadUrl("javascript:window.location.reload(true)");
+                }
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         if(mWebView != null) {
