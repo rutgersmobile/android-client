10420aa46710069e4e4bbb7f9dcbecc58afd52e4
jachambers <jameschambers2@gmail.com>
2014-11-20 12:12:20 -0500

    RSS reader createArgs() method

--- app/src/main/java/edu/rutgers/css/Rutgers/channels/reader/fragments/RSSReader.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/channels/reader/fragments/RSSReader.java
@@ -1,6 +1,7 @@
 package edu.rutgers.css.Rutgers.channels.reader.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,6 +22,7 @@ import org.jdeferred.android.AndroidDeferredManager;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.rutgers.css.Rutgers.Config;
 import edu.rutgers.css.Rutgers.api.ComponentFactory;
 import edu.rutgers.css.Rutgers.api.Request;
 import edu.rutgers.css.Rutgers.channels.reader.model.RSSAdapter;
@@ -30,22 +32,41 @@ import edu.rutgers.css.Rutgers.utils.AppUtils;
 import edu.rutgers.css.Rutgers2.R;
 
 /**
- * RSS Feed display fragment
- * Displays items from an RSS feed.
+ * RSS feed reader
  */
-public class RSSReader extends Fragment implements OnItemClickListener {    
-    
+public class RSSReader extends Fragment implements OnItemClickListener {
+
+    /* Log tag and component handle */
     private static final String TAG = "RSSReader";
     public static final String HANDLE = "reader";
 
+    /* Constants */
+    private static final long EXPIRE = Request.CACHE_ONE_MINUTE;
+
+    /* Argument bundle tags */
+    private static final String ARG_TITLE_TAG       = ComponentFactory.ARG_TITLE_TAG;
+    private static final String ARG_URL_TAG         = ComponentFactory.ARG_URL_TAG;
+
+    /* Saved instance state tags */
+    private static final String SAVED_DATA_TAG      = Config.PACKAGE_NAME + ".reader.data";
+
+    /* Member data */
     private ArrayList<RSSItem> mData;
     private RSSAdapter mAdapter;
-    private long expire = 60 * 1000; // cache feed for one minute
     
     public RSSReader() {
         // Required empty public constructor
     }
-    
+
+    /** Create arugment bundle for an RSS feed reader */
+    public static Bundle createArgs(@NonNull String title, @NonNull String url) {
+        Bundle bundle = new Bundle();
+        bundle.putString(ComponentFactory.ARG_COMPONENT_TAG, RSSReader.HANDLE);
+        bundle.putString(ARG_TITLE_TAG, title);
+        bundle.putString(ARG_URL_TAG, url);
+        return bundle;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,14 +75,13 @@ public class RSSReader extends Fragment implements OnItemClickListener {
         mData = new ArrayList<>();
         mAdapter = new RSSAdapter(this.getActivity(), R.layout.row_rss, mData);
 
-        if(savedInstanceState != null && savedInstanceState.getSerializable("mData") != null) {
-            Log.v(TAG, "Restoring mData");
-            mData.addAll((ArrayList<RSSItem>) savedInstanceState.getSerializable("mData"));
-            mAdapter.notifyDataSetChanged();
+        if(savedInstanceState != null && savedInstanceState.getSerializable(SAVED_DATA_TAG) != null) {
+            Log.d(TAG, "Restoring mData");
+            mAdapter.addAll((ArrayList<RSSItem>) savedInstanceState.getSerializable(SAVED_DATA_TAG));
             return;
         }
 
-        if(args.getString("url") == null) {
+        if(args.getString(ARG_URL_TAG) == null) {
             Log.e(TAG, "RSS feed URL was not set");
             Toast.makeText(getActivity(), R.string.failed_no_url, Toast.LENGTH_SHORT).show();
             return;
@@ -69,7 +89,7 @@ public class RSSReader extends Fragment implements OnItemClickListener {
         
         // Get RSS feed XML and add items through the array adapter\
         AndroidDeferredManager dm = new AndroidDeferredManager();
-        dm.when(Request.xml(args.getString("url"), expire)).done(new DoneCallback<XmlDom>() {
+        dm.when(Request.xml(args.getString(ARG_URL_TAG), RSSReader.EXPIRE)).done(new DoneCallback<XmlDom>() {
             
             @Override
             public void onDone(XmlDom xml) {
@@ -95,17 +115,18 @@ public class RSSReader extends Fragment implements OnItemClickListener {
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_rss_reader, parent, false);
-        ListView listView = (ListView) v.findViewById(R.id.rssreader_list);
-        Bundle args = getArguments();
+
+        final Bundle args = getArguments();
         
-        Log.v(TAG, "RSS Reader launched for " + args.getString("url"));
+        Log.v(TAG, "RSS Reader launched for " + args.getString(ARG_URL_TAG));
         
         // Sets title to name of the RSS feed being displayed
-        if(args.getString("title") != null) {
-            getActivity().setTitle(args.getString("title"));
+        if(args.getString(ARG_TITLE_TAG) != null) {
+            getActivity().setTitle(args.getString(ARG_TITLE_TAG));
         }
 
         // Adapter & click listener for RSS list view
+        ListView listView = (ListView) v.findViewById(R.id.rssreader_list);
         listView.setAdapter(mAdapter);
         listView.setOnItemClickListener(this);
         
@@ -115,7 +136,7 @@ public class RSSReader extends Fragment implements OnItemClickListener {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if(!mData.isEmpty()) outState.putSerializable("mData", mData);
+        if(!mData.isEmpty()) outState.putSerializable(SAVED_DATA_TAG, mData);
     }
 
     @Override
