44dd8b475213c432f3c531b4172b3435497acebf
jamchamb <jameschambers2@gmail.com>
2014-11-25 19:16:49 -0500

    Updating preference keys

--- app/src/main/java/edu/rutgers/css/Rutgers/utils/PrefUtils.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/utils/PrefUtils.java
@@ -4,14 +4,12 @@ package edu.rutgers.css.Rutgers.utils;
  * Preference keys and helpers.
  */
 public final class PrefUtils {
-
     private PrefUtils() {}
 
-    public static final String KEY_PREF_HOME_CAMPUS     = "campus_list";
-    public static final String KEY_PREF_USER_TYPE       = "user_type_list";
-    public static final String KEY_PREF_NOTIFICATIONS   = "notifications_new_message_ringtone";
-    public static final String KEY_PREF_SOC_CAMPUS      = "soc_campus";
-    public static final String KEY_PREF_SOC_LEVEL       = "soc_level";
-    public static final String KEY_PREF_SOC_SEMESTER    = "soc_semester";
-    public static final String KEY_PREFS_FIRST_LAUNCH   = "first_launch";
+    public static final String KEY_PREF_HOME_CAMPUS     = "pref_home_campus";
+    public static final String KEY_PREF_USER_TYPE       = "pref_user_type";
+    public static final String KEY_PREF_SOC_CAMPUS      = "pref_soc_campus";
+    public static final String KEY_PREF_SOC_LEVEL       = "pref_soc_level";
+    public static final String KEY_PREF_SOC_SEMESTER    = "pref_soc_semester";
+    public static final String KEY_PREFS_FIRST_LAUNCH   = "pref_first_launch";
 }
