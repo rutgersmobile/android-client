ff78e6626cf259f9630287731fbe628c442827d3
jamchamb <jameschambers2@gmail.com>
2014-12-06 19:11:35 -0500

    Updating Gradle

--- app/src/main/java/edu/rutgers/css/Rutgers/Config.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/Config.java
@@ -9,7 +9,7 @@ public final class Config {
 
     // Build info
     public static final String APPTAG = "Rutgers";
-    public static final String PACKAGE_NAME = BuildConfig.PACKAGE_NAME;
+    public static final String PACKAGE_NAME = BuildConfig.APPLICATION_ID;
     public static final String VERSION = "4.0";
     public static final String OSNAME = "android";
     public static final String BETAMODE = "dev";
