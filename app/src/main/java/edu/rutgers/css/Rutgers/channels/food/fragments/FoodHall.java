e2fa05451904b2f712fb09cc29674fbea739f5cf
James Chambers <jameschambers2@gmail.com>
2015-02-13 18:00:06 -0500

    Modify foodhall for tab strip

--- app/src/main/java/edu/rutgers/css/Rutgers/channels/food/fragments/FoodHall.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/channels/food/fragments/FoodHall.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.astuetz.PagerSlidingTabStrip;
+
 import org.apache.commons.lang3.time.DatePrinter;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.jdeferred.DoneCallback;
@@ -102,7 +104,7 @@ public class FoodHall extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        final View v = inflater.inflate(R.layout.fragment_pager, parent, false);
+        final View v = inflater.inflate(R.layout.fragment_tabbed_pager, parent, false);
         final Bundle args = getArguments();
 
         if (mTitle != null) {
@@ -117,6 +119,9 @@ public class FoodHall extends Fragment {
 
         final ViewPager viewPager = (ViewPager) v.findViewById(R.id.viewPager);
         viewPager.setAdapter(mPagerAdapter);
+
+        final PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) v.findViewById(R.id.tabs);
+        tabs.setViewPager(viewPager);
 
         return v;
     }
