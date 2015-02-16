cee505d75e8c548d230bc1f38ddd5c3d50709241
James Chambers <jameschambers2@gmail.com>
2015-02-16 17:29:31 -0500

    Fix focus trick

--- app/src/main/java/edu/rutgers/css/Rutgers/channels/bus/fragments/BusMain.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/channels/bus/fragments/BusMain.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,11 +51,11 @@ public class BusMain extends Fragment implements FilterFocusListener {
             getActivity().setTitle(R.string.bus_title);
         }
 
-        final ViewPager viewPager = (ViewPager) v.findViewById(R.id.viewPager);
-        viewPager.setAdapter(new BusFragmentPager(getChildFragmentManager(), this));
+        mViewPager = (ViewPager) v.findViewById(R.id.viewPager);
+        mViewPager.setAdapter(new BusFragmentPager(getChildFragmentManager(), this));
 
         final PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) v.findViewById(R.id.tabs);
-        tabs.setViewPager(viewPager);
+        tabs.setViewPager(mViewPager);
 
         return v;
     }
