058eece0dca38693db0756808fbb96a80350eb30
James Chambers <jameschambers2@gmail.com>
2015-02-27 18:13:29 -0500

    9-patched background

--- app/src/main/java/edu/rutgers/css/Rutgers/ui/fragments/MainScreen.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/ui/fragments/MainScreen.java
@@ -1,8 +1,5 @@
 package edu.rutgers.css.Rutgers.ui.fragments;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.rutgers.css.Rutgers.R;
-import edu.rutgers.css.Rutgers.utils.ImageUtils;
 
 public class MainScreen extends Fragment {
 
@@ -24,26 +20,7 @@ public class MainScreen extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_main_screen, container, false);
-        
         getActivity().setTitle(R.string.app_name);
-
-        // Set background after view is created and size is set
-        v.post(new Runnable() {
-            @Override
-            public void run() {
-                if(!isAdded()) return;
-                // Loads and resizes the background logo
-                Bitmap bg = ImageUtils.decodeSampledBitmapFromResource(getResources(), R.drawable.background, v.getWidth(), v.getHeight());
-                Drawable bgDrawable = new BitmapDrawable(getResources(), bg);
-
-                if (android.os.Build.VERSION.SDK_INT >= 16) {
-                    v.setBackground(bgDrawable);
-                } else {
-                    v.setBackgroundDrawable(bgDrawable);
-                }
-            }
-        });
-
         return v;
     }
 
