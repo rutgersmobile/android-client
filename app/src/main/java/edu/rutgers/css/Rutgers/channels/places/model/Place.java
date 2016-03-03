a925544770269868175b1becfb9f287c8bafa505
James Chambers <jameschambers2@gmail.com>
2016-03-03 14:48:31 -0500

    Improve Places search
    Summary:
    * Updated JavaDocs for PlacesAPI
    * Parameter for capping number of results
    * Removed redundant search behavior
    * Lombokified Place object
    
    Test Plan: Use the places channel. Search for things.
    
    Reviewers: mattro
    
    Reviewed By: mattro
    
    Subscribers: jamchamb
    
    Differential Revision: http://phab.rutgers.edu/D18

--- app/src/main/java/edu/rutgers/css/Rutgers/channels/places/model/Place.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/channels/places/model/Place.java
@@ -4,12 +4,15 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+import lombok.Data;
+
 /**
  * Represents a building from the places database. Equivalent to a "place" JSON object from the
  * places API. Use GSON to construct.
  * @author James Chambers
  */
-public class Place {
+@Data
+public final class Place {
 
     private String id;
     private String title;
@@ -23,7 +26,8 @@ public class Place {
     private List<String> offices;
     private Location location;
 
-    public static class Location {
+    @Data
+    public static final class Location {
 
         private String name;
         private String street;
@@ -36,90 +40,6 @@ public class Place {
         private double latitude;
         private double longitude;
 
-        public String getName() {
-            return name;
-        }
-
-        public String getStreet() {
-            return street;
-        }
-
-        public String getAdditional() {
-            return additional;
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        public String getState() {
-            return state;
-        }
-
-        public String getStateAbbr() {
-            return stateAbbr;
-        }
-
-        public String getPostalCode() {
-            return postalCode;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public double getLatitude() {
-            return latitude;
-        }
-
-        public double getLongitude() {
-            return longitude;
-        }
-
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCid() {
-        return cid;
-    }
-
-    public String getBuildingID() {
-        return buildingID;
-    }
-
-    public String getBuildingNumber() {
-        return buildingNumber;
-    }
-
-    public String getBuildingCode() {
-        return buildingCode;
-    }
-
-    public String getCampusCode() {
-        return campusCode;
-    }
-
-    public String getCampusName() {
-        return campusName;
-    }
-
-    public List<String> getOffices() {
-        return offices;
-    }
-
-    public Location getLocation() {
-        return location;
     }
 
     @Override
