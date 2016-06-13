03611d6713f8414add747698e1bd3f24159fc6a3
Matt Robinson <mattro@oit.rutgers.edu>
2016-06-13 14:38:20 -0400

    Added comments

--- app/src/main/java/edu/rutgers/css/Rutgers/model/BookmarkAdapter.java
+++ app/src/main/java/edu/rutgers/css/Rutgers/model/BookmarkAdapter.java
@@ -31,30 +31,55 @@ import lombok.Data;
  */
 public final class BookmarkAdapter extends BaseAdapter implements Swappable {
 
+    // User created bookmark
     private final int removableResource;
+
+    // Official Rutgers (TM) bookmark
     private final int lockedResource;
+
+    // Divider between enabled / disabled sections
     private final int dividerResource;
+
+    // used to get SharedPreferences to store links
     private final Context context;
+
+    // Links in the drawer + hidden items
     private final List<Link> links;
+
+    // Android bug makes you have to keep a reference to this because otherwise
+    // it gets GC'd for some reason
     private SharedPreferences.OnSharedPreferenceChangeListener listener;
 
+    // View type ids for user created bookmarks, rutgers bookmarks, and the divider
     private static final int REMOVABLE = 0;
     private static final int LOCKED = 1;
     private static final int DIVIDER = 2;
 
+    /**
+     * This function controls what happens when you drag one item over another.
+     * @param positionOne The position that gets dragged to
+     * @param positionTwo The position being dragged from
+     */
     @Override
     public void swapItems(int positionOne, int positionTwo) {
         final int dividerPosition = dividerPosition();
         if (positionOne < dividerPosition && positionTwo < dividerPosition) {
+            // They are both enabled so just swap them
             Collections.swap(links, positionOne, positionTwo);
         } else if (positionOne > dividerPosition && positionTwo > dividerPosition) {
+            // They are both disabled. Just swap them, but remember to subtract for the divider
             Collections.swap(links, positionOne - 1, positionTwo - 1);
         } else if (positionOne == dividerPosition && positionTwo < dividerPosition) {
+            // We're dragging over the divider from above
+            // Don't do anything to user added links since they can only be deleted
+            // TODO This causes a visual bug, but there's probably a fix for it
             final Link link = links.get(positionTwo);
             if (!link.isRemovable()) {
                 link.setEnabled(false);
             }
         } else if (positionOne == dividerPosition && positionTwo > dividerPosition) {
+            // We're dragging over the divider from below. Just enable it
+            // subtract one from the position to account for the divider
             links.get(positionTwo - 1).setEnabled(true);
         }
     }
@@ -64,6 +89,8 @@ public final class BookmarkAdapter extends BaseAdapter implements Swappable {
         return position != dividerPosition();
     }
 
+    // This class will keep references to the view so we don't
+    // have to look them up every time we get a view we've already inflated
     private final class ViewHolder {
         CheckBox checkBox;
         ImageButton imageButton;
@@ -71,12 +98,16 @@ public final class BookmarkAdapter extends BaseAdapter implements Swappable {
         ImageView imageView;
     }
 
+    // Java doesn't have tuples so this is what you have to do if
+    // you want to return multiple values
     @Data
     private final class TwoLists {
         private final List<Link> visible;
         private final List<Link> hidden;
     }
 
+    // This function will split a list of links into a list of visible links and a list of hidden links
+    // We use this to make sure the links are in the right order when we load them
     private TwoLists splitVisible(List<Link> links) {
         final List<Link> visible = new ArrayList<>();
         final List<Link> hidden = new ArrayList<>();
@@ -91,6 +122,8 @@ public final class BookmarkAdapter extends BaseAdapter implements Swappable {
         return new TwoLists(visible, hidden);
     }
 
+    // Find the first disabled link
+    // Everything after the divider is disabled
     private int dividerPosition() {
         for (int i = 0; i < links.size(); i++) {
             if (!links.get(i).isEnabled()) {
@@ -101,6 +134,7 @@ public final class BookmarkAdapter extends BaseAdapter implements Swappable {
         return links.size();
     }
 
+    // Initialize the adapter with bookmarks saved in the preferences
     public void addFromPrefs() {
         final List<Link> links = PrefUtils.getBookmarks(context);
         final TwoLists lists = splitVisible(links);
@@ -115,6 +149,7 @@ public final class BookmarkAdapter extends BaseAdapter implements Swappable {
         this.dividerResource = dividerResource;
         this.links = new ArrayList<>();
 
+        // Update persistent storage when the list is updated
         registerDataSetObserver(new DataSetObserver() {
             @Override
             public void onChanged() {
@@ -123,6 +158,7 @@ public final class BookmarkAdapter extends BaseAdapter implements Swappable {
             }
         });
 
+        // When the bookmarks are changed, reload the drawer
         listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
@@ -253,6 +289,10 @@ public final class BookmarkAdapter extends BaseAdapter implements Swappable {
                     public void onClick(View v) {
                         CheckBox checkBox = (CheckBox) v;
                         link.setEnabled(checkBox.isChecked());
+
+                        // We need to move the link between sections so we just insert it at the divider position
+                        // if it was enabled it will go to the end of the enabled section
+                        // otherwise it goes to the beginning of the disabled section
                         removeNoNotify(position);
                         links.add(dividerPosition(), link);
                         notifyDataSetChanged();
