10eb9b694623b047799de1e064b5f3f0fbd60a84
Matt Robinson <mattro@oit.rutgers.edu>
2016-10-03 09:33:21 -0400

    Added SimpleSectionRecyclerAdapter for Food and others

---
+++ app/src/main/java/edu/rutgers/css/Rutgers/model/SimpleSectionedRecyclerAdapter.java
@@ -0,0 +1,8 @@
+package edu.rutgers.css.Rutgers.model;
+
+/**
+ * Created by mattro on 9/30/16.
+ */
+
+public class SimpleSectionedRecyclerAdapter {
+}
---
+++ app/src/main/java/edu/rutgers/css/Rutgers/utils/FuncUtils.java
@@ -0,0 +1,8 @@
+package edu.rutgers.css.Rutgers.utils;
+
+/**
+ * Created by mattro on 9/30/16.
+ */
+
+public class FuncUtils {
+}
